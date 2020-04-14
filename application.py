fcb856eccdf56877c5d7bd09dcc6974f0a30f98d
Russ Sayers <russell.sayers@gmail.com>
2020-04-13 17:23:56 -0700

    added ProxyFix now that we are proxied behind nginx https://werkzeug.palletsprojects.com/en/1.0.x/middleware/proxy_fix/

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask, escape, request
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/')
 def hello():
