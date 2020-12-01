b2fce1ae6a36d39d6fc1f992a4c45d121728373d
Russ Sayers <russell.sayers@gmail.com>
2020-12-01 15:25:32 +1100

    I think AWS has changed the WSGIPath default to application

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask, escape, request
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
+application = Flask(__name__)
+application.wsgi_app = ProxyFix(application.wsgi_app)
 
-@app.route('/')
+@application.route('/')
 def hello():
     name = request.args.get("name", "World")
     greeting = "Secure hello" if request.url.startswith('https://') else "Non-secure hello"
